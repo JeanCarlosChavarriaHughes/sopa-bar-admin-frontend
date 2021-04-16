fde359ec7902d5ae2ea7d995585694b7bebbf02c
JeanCarlos Chavarria Hughes <jchavarr@akamai.com>
2021-04-16 14:37:10 -0600

    Segregating this repo to only frontend

--- backend/src/server.ts
+++
@@ -1,9 +0,0 @@
-import env from './environment';
-import express from 'express';
-const app = express();
-
-const PORT = env.getPort();
-app.get('/', (req, res) => res.json({ Puerto: PORT }));
-app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
